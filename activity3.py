aa3c54713dd53d8e044aa09042d198354b01ccdf
Soha564 <G23ns1@cchs.essex.sch.uk>
2025-03-16 16:19:26 +0000

    activity1 completed

---
+++ activity3.py
@@ -0,0 +1,17 @@
+print("YOUR VERY OWN SPELL GENERATOR")
+
+magical_word = input("Enter your magical word:")
+
+firstspell = magical_word.upper
+
+secondspell = magical_word * 7 
+
+thirdspell = magical_word [ :4]
+
+forthspell = magical_word [::-1]
+
+fifthspell = firstspell, thirdspell
+
+finalspell = fifthspell,  forthspell
+
+print(finalspell)
