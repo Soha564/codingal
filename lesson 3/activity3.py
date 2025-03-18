90f1b6ca84bc8131b90b2e4c33bc34c1395ecab8
Soha564 <G23ns1@cchs.essex.sch.uk>
2025-03-18 10:41:20 +0000

    activity3 completed

--- lesson 3/activity3.py
+++ lesson 3/activity3.py
@@ -2,14 +2,12 @@ print("YOUR VERY OWN SPELL GENERATOR")
 
 magical_word = input("Enter your magical word:")
 
-firstspell = magical_word.upper()
+firstspell = magical_word.upper
 
 secondspell = magical_word * 7 
 
 thirdspell = magical_word [ :4]
 
-forthspell = firstspell + " " + thirdspell
-
-finalspell = forthspell + " " +  secondspell 
+finalspell = thirdspell + " " + secondspell
 
 print(finalspell)
