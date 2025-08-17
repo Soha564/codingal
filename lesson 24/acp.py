1a77eb24bfe5f24e2d53a7dd76a7aaaa79b710e3
Soha564 <G23ns1@cchs.essex.sch.uk>
2025-08-17 14:14:46 +0100

    acp completed

---
+++ lesson 24/acp.py
@@ -0,0 +1,17 @@
+#A
+Set1 = {'green', 'blue'} 
+Set2 = {'blue', 'yellow'}
+print("Set 1:", Set1)
+print("Set 2:", Set2)
+
+print("Symmetric Difference :", Set1.symmetric_difference(Set2))
+
+print("\n")
+#B
+Set1 = {1, 2, 3, 4, 5} 
+Set2 = {1, 5, 6, 7, 8, 9}
+print("Set 1:", Set1)
+print("Set 2:", Set2)
+print("Symmetric Difference :", Set1.symmetric_difference(Set2))
+
+print("\n")
